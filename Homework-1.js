693980af1d6b329fbd7a0f6b1f2ceb8815f96398
gor017 <85189675+gor017@users.noreply.github.com>
2021-08-04 12:19:56 +0400

    Update Homework-1.js

--- Homework-1.js
+++ Homework-1.js
@@ -95,3 +95,15 @@ function thirdEl (str) {
     return str
 }
 
+//buble sort-i banadzev
+let arr = [8,3,2,4,6,1,7,9,5]
+for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] > arr[i + 1]) {
+            let a = arr[i]
+            arr[i] = arr[i + 1]
+            arr[i + 1] = a
+        }
+    }
+}
+console.log(arr)
